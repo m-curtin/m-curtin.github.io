8a6d8597f5945e54c1a38b0ac0167e2e58c1d05b
Nonentityboy <1138580559@qq.com>
2020-11-30 01:18:23 +0800

    666

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 echo "-------Begin-------"
 git add .
-read -p "提交信息:" commitinfo
+read -p "Curtin, coding your message:" commitinfo
 git commit -m $commitinfo
-
-
 git push -f git@github.com:allspark-fe/knowledge-graph.git main
-
-cd -
+echo "-------End-------"
