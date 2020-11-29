21b08b7c4e22cc4f3c4ecbc80f940dd45fca7aa0
Nonentityboy <1138580559@qq.com>
2020-11-30 01:22:23 +0800

    改变changesh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ echo "-------Begin-------"
 git add .
 read -p "Curtin, coding your message:" commitinfo
 git commit -m $commitinfo
-git push -f git@github.com:allspark-fe/knowledge-graph.git main
+git push origin main
 echo "-------End-------"
