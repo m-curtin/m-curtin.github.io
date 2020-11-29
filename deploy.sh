dba44b64f4e709924dc29a8037e0a7ad823b6700
Nonentityboy <1138580559@qq.com>
2020-11-30 01:07:42 +0800

    112

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "-------Begin-------"
-git status
-git add -A
+git add .
 read -p "提交信息:" commitinfo
 git commit -m $commitinfo
-git pull –-rebase origin master
-git push origin master
 echo "--------End--------"
+git pull origin master
+git push origin master
