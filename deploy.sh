2a7a8874aa3586ebda16d5f9cd8cf5cbd774570b
Nonentityboy <1138580559@qq.com>
2020-11-30 01:17:12 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ read -p "提交信息:" commitinfo
 git commit -m $commitinfo
 
 
-git push -f git@github.com:allspark-fe/knowledge-graph.git master
+git push -f git@github.com:allspark-fe/knowledge-graph.git main
 
 cd -
