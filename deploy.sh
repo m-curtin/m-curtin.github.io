184d3d25cdfacd41df5fcbf47379719a8161c19f
Nonentityboy <1138580559@qq.com>
2020-11-30 00:58:45 +0800

    hah

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
+#!/bin/bash 注释 暂时有问题CMD不能到git里面来
+echo "-------Begin-------"
+git status
 git add .
-
-git commit -m 'deploy'
-
-git push origin master
+read -p "提交信息:" commitinfo
+git commit -m $commitinfo
+git push -u origin master
+echo "--------End--------"
+read -p "Enter a number:" num
