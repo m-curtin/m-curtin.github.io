81f2772e7b1f3d65c94f324b22636fcf5a8b3d20
Nonentityboy <1138580559@qq.com>
2020-11-30 01:13:50 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@ echo "-------Begin-------"
 git add .
 read -p "提交信息:" commitinfo
 git commit -m $commitinfo
-echo "--------End--------"
 git remote add origin git@github.com:allspark-fe/knowledge-graph.git
 git push origin master
