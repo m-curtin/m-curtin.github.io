ad833f5373af6953d95505ae755caa73a8fb3418
Nonentityboy <1138580559@qq.com>
2020-11-30 12:42:58 +0800

    "update"

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "\033[46;37m ----------------------------Begin---------------------------- \033[0m"
 git add .
-read -p "\033[34m Curtin, coding your message:\033[0m" commitInfo
+echo "\033[34m Curtin, coding your message: \033[0m"
+read -p "Please: " commitInfo
 git commit -m $commitInfo
 git push origin main
 echo "\033[46;37m ----------------------------End---------------------------- \033[0m"
